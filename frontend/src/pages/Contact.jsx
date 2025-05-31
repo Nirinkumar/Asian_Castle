f196dcb214a7f02a9e7d24bd1b23f8bf02e5a879
Nirinkumar <nirinkumar18@gmail.com>
2025-05-31 15:04:01 +0530

    contact page added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,25 @@
 export default function Contact(){
     return(
-        <div>Contact</div>
+        <div className="contact">
+            <div class="contact-container">
+                <h2>Contact Us</h2>
+                <form class="contact-form">
+        
+                    <label for="name">Name<span>*</span></label>
+                    <input type="text" name="name" id="name" placeholder="enter name" required/>
+
+                    <label for="email">Email<span>*</span></label>
+                    <input type="email" name="email" id="email" placeholder="enter email" required />
+
+                    <label for="message">Message:</label>
+                    <textarea className="form-message" name="message" id="message" placeholder="Write a message.." />
+
+                    <button className="submit-btn">Submit</button>
+                    {/* <div class="submit-btn"> */}
+                        {/* <input type="submit" className="submit-input" /> */}
+                    {/* </div> */}
+                </form>
+            </div>
+        </div>   
     )
 }
