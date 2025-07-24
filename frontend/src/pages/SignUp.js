d1a9700105fdb1a4c4644f68e17f6e2a35c23be2
Nirinkumar <nirinkumar18@gmail.com>
2025-07-24 13:23:30 +0530

    signup updated

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -10,7 +10,7 @@ export default function SignUp() {
         name : "",
         age : "",
         address : "",
-        // gender : "",
+        gender : "",
         mobileNo: "",
         email: "",
         username: "",
@@ -28,9 +28,9 @@ export default function SignUp() {
         await axios.post("http://localhost:8080/user/register",user)
     }
 
-    function handleGender(value){
-        setGender(value);
-    }
+    // function handleGender(value){
+    //     setGender(value);
+    // }
   return (
     <div className='sign-up'>
         <div className="login-container">
@@ -42,15 +42,46 @@ export default function SignUp() {
                 <input type='number' name='age' value={age} placeholder='Enter your age' onChange={(e)=>onInputChange(e)} required />
                 <label>Address</label>
                 <input type='text' name='address' value={address} placeholder='Enter your Full address' onChange={(e)=>onInputChange(e)} required />
-                <label>Gender</label>
-                    <div className='gender-option'>
-                        <input type='radio' id='male' name='gender' value="male" 
-                        onChange={()=>{
-                            handleGender("male")
-                        }}/>
-                        <p htmlFor="male" className='gender-category'>Male</p>
+              
+                {/* <select name="gender" onChange={(e)=>onInputChange(e)} required>
+                    <option value="">Select Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select><br /> */}
 
-                        <input type='radio' id='female' name='gender' value="female" 
+                <div>
+                    <label>Gender</label>
+                    <div className='gender-option'>
+                                           
+                        <input 
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            checked={user.gender === 'Male'} 
+                            onChange={(e)=>onInputChange(e)}
+                        />
+                        <p htmlFor="male" className='gender-category'>Male</p>
+                                          
+                        <input 
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            checked={user.gender === 'Female'} 
+                            onChange={(e)=>onInputChange(e)}
+                        />
+                        <p htmlFor="female" className='gender-category'>Female</p>
+                                       
+                        <input 
+                            type="radio"
+                            name="gender"
+                            value="others"
+                            checked={user.gender === 'Others'} 
+                            onChange={(e)=>onInputChange(e)}
+                        />
+                        <p htmlFor="others" className='gender-category'>Others</p>
+                        
+
+                        {/* <input type='radio' id='female' name='gender' value="female" 
                         onChange={()=>{
                             handleGender("female")
                         }}/>
@@ -60,8 +91,9 @@ export default function SignUp() {
                         onChange={()=>{
                             handleGender("others")
                         }}/>
-                        <p htmlFor="others" className='gender-category'>Others</p>
+                        <p htmlFor="others" className='gender-category'>Others</p> */}
                     </div>
+                </div>
                 <label>Mobile No</label>
                 <input type="number" name="mobileNo" value={mobileNo} placeholder="Enter your mobile no" onChange={(e)=>onInputChange(e)} required />    
                 <label>Email</label>
