2a73f5cfc607978c54349f340b36144a6f816781
Nirinkumar <nirinkumar18@gmail.com>
2025-07-29 02:12:21 +0530

    contact page updated

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,4 +1,31 @@
+import { React , useState } from "react"
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
+
+
 export default function Contact(){
+
+    let navigate = useNavigate()
+
+    const [feed, setFeed] = useState({
+        name:"",
+        email:"",
+        message:""
+    })
+
+    const{name, email, message} = feed
+
+    const onInputChange=(e)=>{
+        setFeed({...feed,[e.target.name]:e.target.value})
+    }
+
+    const onSubmit = async (e)=>{
+        e.preventDefault();
+        await axios.post("http://localhost:8080/feedback", feed)
+        alert("Message sent successfully!!!")
+        navigate('/')
+    }
+
     return(
         <div className="contact">
             <div class="contact-container">
@@ -6,15 +33,36 @@ export default function Contact(){
                 <form class="contact-form">
         
                     <label for="name">Name<span>*</span></label>
-                    <input type="text" name="name" id="name" placeholder="enter name" required/>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        id="name" 
+                        placeholder="enter name"  
+                        value={name}
+                        onChange={(e)=>onInputChange(e)}
+                        required/>
 
                     <label for="email">Email<span>*</span></label>
-                    <input type="email" name="email" id="email" placeholder="enter email" required />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="enter email" 
+                        value={email}
+                        onChange={(e)=>onInputChange(e)}
+                        required />
 
-                    <label for="message">Message:</label>
-                    <textarea className="form-message" name="message" id="message" placeholder="Write a message.." />
+                    <label for="message">Message<span>*</span></label>
+                    <textarea 
+                        className="form-message" 
+                        name="message" 
+                        id="message" 
+                        placeholder="Write a message.." 
+                        value={message}
+                        onChange={(e)=>onInputChange(e)}
+                        required/>
 
-                    <button className="submit-btn">Submit</button>
+                    <button className="submit-btn" onClick={onSubmit}>Submit</button>
                     {/* <div class="submit-btn"> */}
                         {/* <input type="submit" className="submit-input" /> */}
                     {/* </div> */}
